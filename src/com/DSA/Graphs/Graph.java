87089d710bee93ec90e06629a28f2f24e4de5a63
Jay Gheewala <jaygheewala157@gmail.com>
2024-11-02 10:17:17 +0530

    topological sort using DFS and Kahn's Algorithm(BFS)

--- src/com/DSA/Graphs/Graph.java
+++ src/com/DSA/Graphs/Graph.java
@@ -1,30 +1,28 @@
 package com.DSA.Graphs;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
 
 class Graph<T> {
 
     private final HashMap<T, List<T>> adjacencyList = new HashMap<>();
+    public boolean isDirected;
 
-    public void addEdge(T u, T v, boolean direction) {
-        //direction = true -> directed graph
-        //direction = false -> undirected graph
+    public Graph(boolean isDirected) {
+        this.isDirected = isDirected;
+    }
 
+    public void addEdge(T u, T v) {
         adjacencyList.putIfAbsent(u, new ArrayList<>());
         adjacencyList.get(u).add(v);
 
-        if (!direction) {
+        if (!isDirected) {
             adjacencyList.putIfAbsent(v, new ArrayList<>());
             adjacencyList.get(v).add(u);
         }
     }
 
     public void printAdjList() {
+        System.out.println("Adjacency List :-");
         for (T key : adjacencyList.keySet()) {
             System.out.print(key + " -> ");
             for (T edge : adjacencyList.get(key)) {
@@ -36,7 +34,7 @@ class Graph<T> {
 
     public void bfs() {
         HashSet<T> visited = new HashSet<>();
-
+        System.out.print("BFS :- ");
         for(T key : adjacencyList.keySet()) {
             if (!visited.contains(key)) {
                 bfs(key, visited);
@@ -63,7 +61,7 @@ class Graph<T> {
 
     public void dfs() {
         HashSet<T> visited = new HashSet<>();
-
+        System.out.print("DFS :- ");
         for (T key: adjacencyList.keySet()) {
             if (!visited.contains(key)) {
                 dfs(key, visited);
@@ -80,7 +78,16 @@ class Graph<T> {
         }
     }
 
-    public boolean detectCycleBFS() {
+    public void detectCycle() {
+        if (!isDirected) {
+            System.out.println("Detect Cycle BFS :- " + detectCycleBFS());
+            System.out.println("Detect Cycle DFS :- " + detectCycleDFS());
+        } else {
+            System.out.println("Detect Cycle DFS :- " + detectCycleDirectedDFS());
+        }
+    }
+
+    private boolean detectCycleBFS() {
         HashSet<T> visited = new HashSet<>();
         HashMap<T, T> parent = new HashMap<>();
 
@@ -114,7 +121,7 @@ class Graph<T> {
         return false;
     }
 
-    public boolean detectCycleDFS() {
+    private boolean detectCycleDFS() {
         HashSet<T> visited = new HashSet<>();
 
         for(T key : adjacencyList.keySet()) {
@@ -141,25 +148,25 @@ class Graph<T> {
         return false;
     }
 
-    public boolean detectCycleDirected() {
+    private boolean detectCycleDirectedDFS() {
         HashSet<T> visited = new HashSet<>();
 
         for (T key : adjacencyList.keySet()) {
             if (visited.contains(key)) continue;
-            if (detectCycleDirected(key, visited, new HashSet<>())) {
+            if (detectCycleDirectedDFS(key, visited, new HashSet<>())) {
                 return true;
             }
         }
         return false;
     }
 
-    private boolean detectCycleDirected(T node, HashSet<T> visited, HashSet<T> recursionStack) {
+    private boolean detectCycleDirectedDFS(T node, HashSet<T> visited, HashSet<T> recursionStack) {
         visited.add(node);
         recursionStack.add(node);
 
         for (T neighbour : adjacencyList.getOrDefault(node, new ArrayList<>())) {
             if (!visited.contains(neighbour)) {
-                if (detectCycleDirected(neighbour, visited, recursionStack)) {
+                if (detectCycleDirectedDFS(neighbour, visited, recursionStack)) {
                     return true;
                 }
             } else if (recursionStack.contains(neighbour)) {
@@ -168,5 +175,82 @@ class Graph<T> {
         }
         recursionStack.remove(node);
         return false;
+    }
+
+    /*
+    * Topological Sort
+    * Can be performed only on Directed Acyclic Graphs
+    * It is linear order of vertices such that
+    * for every edge u -> v,
+    * u always appears before v in that ordering
+    */
+
+    public void topologicalSort() {
+        if (!isDirected) {
+            System.out.println("Graph must be a directed graph");
+            return;
+        }
+        if (detectCycleDirectedDFS()) {
+            System.out.println("Graph must be a acyclic graph");
+            return;
+        }
+        System.out.println("Topological Sort :-");
+        System.out.print("DFS :- ");
+        topologicalSortDFS();
+        System.out.print("\nKahn's Algorithm(BFS) :- ");
+        topologicalSortBFS();
+        System.out.println();
+    }
+
+    private void topologicalSortDFS() {
+        HashSet<T> visited = new HashSet<>();
+        Stack<T> s = new Stack<>();
+
+        for (T node : adjacencyList.keySet()) {
+            if (!visited.contains(node)) {
+                topologicalSortDFS(node, visited, s);
+            }
+        }
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + ", ");
+        }
+    }
+
+    private void topologicalSortDFS(T node, HashSet<T> visited, Stack<T> s) {
+        visited.add(node);
+        for (T neighbour : adjacencyList.getOrDefault(node, new ArrayList<>())) {
+            if (!visited.contains(neighbour)) {
+                topologicalSortDFS(neighbour, visited, s);
+            }
+        }
+        s.push(node);
+    }
+
+    // Kahn's Algorithm (BFS)
+
+    private void topologicalSortBFS() {
+        HashMap<T, Integer> inDegree = new HashMap<>();
+
+        for (T node : adjacencyList.keySet()) {
+            for (T neighbour : adjacencyList.getOrDefault(node, new ArrayList<>())) {
+                inDegree.put(neighbour, inDegree.getOrDefault(neighbour, 0) + 1);
+            }
+        }
+        Queue<T> queue = new LinkedList<>();
+        for (T node : adjacencyList.keySet()) {
+            if (inDegree.getOrDefault(node, 0) == 0)
+                queue.add(node);
+        }
+
+        while (!queue.isEmpty()) {
+            T front = queue.poll();
+            System.out.print(front + ", ");
+
+            for (T neighbour : adjacencyList.getOrDefault(front, new ArrayList<>())) {
+                inDegree.put(neighbour, inDegree.get(neighbour) - 1);
+                if (inDegree.get(neighbour) == 0)
+                    queue.add(neighbour);
+            }
+        }
     }
 }
--- src/com/DSA/Graphs/Main.java
+++ src/com/DSA/Graphs/Main.java
@@ -4,22 +4,24 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Graph<Integer> graph = new Graph<>();
         Scanner s = new Scanner(System.in);
+        System.out.print("Is it Directed Graph?(true/false) :- ");
+        boolean directed = s.nextBoolean();
         System.out.print("Enter number of Edges :- ");
         int edges = s.nextInt();
-        System.out.println();
+        Graph<Integer> graph = new Graph<>(directed);
+        System.out.println("Enter Edges :- ");
         for (int i = 0; i < edges; i++) {
             int u = s.nextInt();
             int v = s.nextInt();
-            graph.addEdge(u, v, true);
+            graph.addEdge(u, v);
         }
         graph.printAdjList();
         graph.bfs();
         System.out.println();
         graph.dfs();
         System.out.println();
-        System.out.println(graph.detectCycleDirected());
-        System.out.println(graph.detectCycleDirected());
+        graph.detectCycle();
+        graph.topologicalSort();
     }
 }
